2f1283ea70e87e9ed53d69de161bc3305d6179bf
yjoonho <111098587+yjoonho@users.noreply.github.com>
2025-02-28 14:50:09 +0900

    Update streamlit_app.py
    Make a Version of my_dataframe, but call it pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,13 @@ session = cnx.session()
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC Function
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 import streamlit as st
