7b11ce42eb2d0205632c0bacb919d895f893fc8b
yjoonho <111098587+yjoonho@users.noreply.github.com>
2025-02-28 14:56:04 +0900

    Update streamlit_app.py
    Making Use of the SEARCH_ON Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,10 +86,11 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+#        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Inoformation')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+#        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         # The += operator means "add this to what is already in the variable" so each time the FOR Loop is repeated
     st.write(ingredients_string)
